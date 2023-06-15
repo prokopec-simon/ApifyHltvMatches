ac0188bcdaba8988c102120ed357cddd75bb8d9e
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-06-15 09:17:16 +0200

    Major refactor

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,19 @@
-import { Actor } from "apify";
-import { CheerioCrawler } from "crawlee";
+import { Actor } from 'apify';
+import { CheerioCrawler } from 'crawlee';
 
 await Actor.init();
-const matchesUrl = "https://www.hltv.org/matches/";
-const startUrls = [matchesUrl];
 const proxyConfiguration = await Actor.createProxyConfiguration();
 
 const crawler = new CheerioCrawler({
     proxyConfiguration,
     async requestHandler({ $ }) {
-        const pageContent = $("html").html();
+        const pageContent = $('html').html();
         await Actor.pushData({
-            fullUrl: matchesUrl,
-            pageContent: pageContent,
+            status: 200,
+            data: [{ rawPageContent: pageContent }],
         });
     },
 });
 
-await crawler.run(startUrls);
+await crawler.run(['https://www.hltv.org/matches/']);
 await Actor.exit();
